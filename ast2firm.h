678b596e15fa892a3166a6d33a952d6208723875
Matthias Braun <matze@braunis.de>
2007-11-12 19:48:52 +0000

    add missing files
    [r18380]

---
+++ ast2firm.h
@@ -0,0 +1,15 @@
+#ifndef AST2FIRM_H
+#define AST2FIRM_H
+
+#include "ast.h"
+
+void translation_unit_to_firm(translation_unit_t *unit);
+
+void init_ast2firm(void);
+void exit_ast2firm(void);
+
+const char *retrieve_dbg(const dbg_info *dbg, unsigned *line);
+ir_node *uninitialized_local_var(ir_graph *irg, ir_mode *mode, int pos);
+unsigned dbg_snprint(char *buf, unsigned len, const dbg_info *dbg);
+
+#endif
---
+++ write_fluffy.h
@@ -0,0 +1,8 @@
+#ifndef WRITE_FLUFFY_H
+#define WRITE_FLUFFY_H
+
+#include "ast.h"
+
+void write_fluffy_decls(const translation_unit_t *unit);
+
+#endif
