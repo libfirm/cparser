b7a6d9e60f17449c2edb95ff2740c97dbe2e06c4
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-11-17 22:45:27 +0000

    add NORETURN
    [r18469]

--- config.h
+++ config.h
@@ -1,5 +1,11 @@
 #define INLINE inline
 
-#ifndef __GNUC__
+#ifdef __GNUC__
+#define NORETURN __attribute__((noreturn))
+#elseif defined(_MSC_VER)
+#define NORETURN __declspec(noreturn)
+#define __attribute__(x)
+#else
+#define NORETURN
 #define __attribute__(x)
 #endif
