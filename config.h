d25522be2467160d61327fef47c4d564d59340e9
Christoph Mallon <christoph.mallon@gmx.de>
2007-11-18 06:53:14 +0000

    s/#elseif/#elif/.
    [r18473]

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 #ifdef __GNUC__
 #define NORETURN __attribute__((noreturn))
-#elseif defined(_MSC_VER)
+#elif defined _MSC_VER
 #define NORETURN __declspec(noreturn)
 #define __attribute__(x)
 #else
