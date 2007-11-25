f282f53a000c79de4f94328e1bd6bd770aa6e446
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2007-11-25 18:12:06 +0000

    use NORETURN macro instead of gcc like __attribute__(noreturn)
    [r18526]

--- adt/error.h
+++ adt/error.h
@@ -1,7 +1,8 @@
 // placeholder file...
 #include <stdio.h>
 #include <stdlib.h>
+#include "config.h"
 
-static inline __attribute__((noreturn))
+static inline NORETURN
 void panic(const char *msg)
 { fprintf(stderr, "Panic: %s\n", msg); abort(); }
