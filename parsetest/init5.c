50375e49dd2f237b3f8e28c66905c3374af19878
Matthias Braun <matze@braunis.de>
2008-02-15 11:15:16 +0000

    more tricky initialiser tests
    [r18865]

--- parsetest/init5.c
+++ parsetest/init5.c
@@ -11,7 +11,18 @@ struct S {
 
 struct S arr[] = { 1, (struct k) { 4, 5 } };
 
+typedef int k[];
+
+k var = { 1, 2, 3, 4, 5 };
+k var2 = { 1, 2, 3, 4 };
+
 int main(void)
 {
+	if((sizeof(var2)/sizeof(int)) != 4)
+		return 1;
+
+	if((sizeof(var)/sizeof(int)) != 5)
+		return 2;
+
 	return 0;
 }
