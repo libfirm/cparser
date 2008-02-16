88e2bca78fe13183319b063bb7b00b4d1a655ef6
Matthias Braun <matze@braunis.de>
2008-02-16 17:42:39 +0000

    it gets even more strange
    [r18885]

--- parsetest/vla.c
+++ parsetest/vla.c
@@ -6,12 +6,24 @@ int get_size(void)
 	return 10;
 }
 
-int main(void)
+int get_size2(void)
+{
+	puts("sideeffect2");
+	return 10;
+}
+
+int main(int argc, char **argv)
 {
 	int k[get_size()];
+	int c[argc][argc];
+	int z[10];
 
 	if(sizeof(k)/sizeof(int) != 10)
 		return 1;
+	if(sizeof(int[get_size2()])/sizeof(int) != 10)
+		return 2;
+	sizeof(c[puts("effect")]);
+	sizeof(z[puts("no effect")]);
 
 	return 0;
 }
