ffaec83406ffaee8b981b2b80114bc2e2eab5f0e
Matthias Braun <matze@braunis.de>
2008-02-18 15:08:52 +0000

    add license prefix
    [r18894]

--- adt/obst.h
+++ adt/obst.h
@@ -1,10 +1,21 @@
-/**
- * @file
- * @date    17.03.2007
- * @brief   Makes using obstack.h easier by including obstack_chunk_alloc and
- *          obstack_chunk_free
- * @author  Martin Trapp, Christian Schaefer
- * @version $Id$
+/*
+ * This file is part of cparser.
+ * Copyright (C) 2007-2008 Matthias Braun <matze@braunis.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
  */
 #include "obstack.h"
 #include "xmalloc.h"
