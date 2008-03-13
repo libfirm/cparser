4a24d06907fc5e3a74b3fb7cb2aa628e7ff4fba5
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-03-13 16:04:24 +0000

    test the build version
    [r18933]

--- parsetest/do_tests.sh
+++ parsetest/do_tests.sh
@@ -3,7 +3,7 @@
 rm -f messages.cparser messages.gcc
 for i in *.c; do
 	echo -n "Compile $i..."
-	cparser $i -O3 -o prog.cparser >> messages.cparser 2>&1 || echo -n " CPARSER COMPILE FAILED"
+	../build/cparser $i -O3 -o prog.cparser >> messages.cparser 2>&1 || echo -n " CPARSER COMPILE FAILED"
 	gcc -m32 -std=gnu99 $i -O3 -o prog.gcc >> messages.gcc 2>&1 || echo -n " GCC COMPILE FAILED"
 	./prog.cparser > out.cparser || echo -n " FAILED CPARSER RUN"
 	./prog.gcc > out.gcc || echo -n " FAILED GCC RUN"
