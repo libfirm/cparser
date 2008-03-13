ae44007754158f29c7c881562d2166a31e85d143
Matthias Braun <matze@braunis.de>
2008-03-13 14:34:40 +0000

    improve testscript
    [r18927]

--- parsetest/do_tests.sh
+++ parsetest/do_tests.sh
@@ -4,9 +4,16 @@ rm -f messages.cparser messages.gcc
 for i in *.c; do
 	echo -n "Compile $i..."
 	../cparser $i -O3 -o prog.cparser >> messages.cparser 2>&1
-	gcc $i -O3 -o prog.gcc >> messages.gcc 2>&1
-	./prog.cparser > out.cparser
-	./prog.gcc > out.gcc
-	diff -u out.cparser out.gcc > /dev/null || echo -n " FAILED"
+	gcc -std=gnu99 $i -O3 -o prog.gcc >> messages.gcc 2>&1
+	./prog.cparser > out.cparser || echo -n " FAILED CPARSER RUN"
+	./prog.gcc > out.gcc || echo -n " FAILED GCC RUN"
+	diff -u out.cparser out.gcc > /dev/null || echo -n " RESULTS MISCOMPARE"
+	echo ""
+done
+
+for i in shouldfail/*.c; do
+	echo -n "Compile $i..."
+	../cparser $i -O3 -o prog.cparser >> messages.cparser 2>&1 && echo -n " CPARSER COMPILED"
+	gcc -std=gnu99 $i -O3 -o prog.gcc >> messages.gcc 2>&1 && echo -n " GCC COMPILED"
 	echo ""
 done
