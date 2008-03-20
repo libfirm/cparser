46d85a597051431088cb9eb92fa0e093c49a12c1
Matthias Braun <matze@braunis.de>
2008-03-20 09:16:46 +0000

    some struct namespacing tests
    [r18999]

---
+++ parsetest/shouldfail/structns0.c
@@ -0,0 +1,9 @@
+struct L { int a; };
+
+int main(void)
+{
+	struct L;
+	struct L *k;
+
+	return k->a;
+}
---
+++ parsetest/structns1.c
@@ -0,0 +1,8 @@
+struct L { int a; };
+
+int main(void)
+{
+	struct L k = { 0 };
+
+	return k.a;
+}
