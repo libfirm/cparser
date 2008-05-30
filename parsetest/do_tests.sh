f8a6ccf65ffba0d26575ac4e9c18587459e0b2d6
Matthias Braun <matze@braunis.de>
2008-05-30 11:17:34 +0000

    reorganized some test programs
    [r19832]

--- parsetest/do_tests.sh
+++ parsetest/do_tests.sh
@@ -18,6 +18,19 @@ for i in MS/*.c; do
 	echo ""
 done
 
+for i in should_warn/*.c; do
+	echo -n "Compile $i..."
+	../cparser $i -Wall -O3 -o prog.cparser >> messages.cparser 2>&1 || echo -n " CPARSER DID NOT COMPILE"
+	gcc -Wall -m32 -std=gnu99 $i -O3 -o prog.gcc >> messages.gcc 2>&1 || echo -n " GCC DID NOT COMPILER"
+	echo ""
+	if diff -u /dev/null messages.cparser > /dev/null; then
+		echo -n " CPARSER HAD NO WARNINGS"
+	fi
+	if diff -u /dev/null messages.gcc > /dev/null; then
+		echo -n " GCC HAD NO WARNINGS"
+	fi
+done
+
 for i in shouldfail/*.c; do
 	echo -n "Compile $i..."
 	../cparser $i -O3 -o prog.cparser >> messages.cparser 2>&1 && echo -n " CPARSER COMPILED"
