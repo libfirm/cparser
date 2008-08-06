70724679e349298ee399b49a9157f07c4d323c79
Matthias Braun <matze@braunis.de>
2008-08-06 10:45:00 +0000

    more tests
    [r21010]

--- parsetest/constexpr2.c
+++ parsetest/constexpr2.c
@@ -2,7 +2,7 @@ struct foo {
 	int a;
 	int b;
 };
-int glob = (int) &((struct foo*)0)->b;
+struct foo bar = { (int) &((struct foo*)0)->b, 0 };
 
 int main(void) {
 	return 0;
---
+++ parsetest/gnu99/attributes2.c
@@ -0,0 +1,5 @@
+__attribute__((noreturn)) void d0(void), __attribute__((format(printf,1,2))) d1(const char*, ...), d2(void);
+
+int main(void) {
+	return 0;
+}
