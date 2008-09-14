acd1bd5e2ef817b3c4dd5ad765bb54b7e33a715c
Christoph Mallon <christoph.mallon@gmx.de>
2008-09-14 15:12:27 +0000

    Fix parsing of function declarations with redundant parentheses around the declarator name.
    [r21940]

--- parser.c
+++ parser.c
@@ -4032,8 +4032,10 @@ static construct_type_t *parse_inner_declarator(declaration_t *declaration,
 		next_token();
 		add_anchor_token(')');
 		inner_types = parse_inner_declarator(declaration, may_be_abstract);
-		/* All later declarators only modify the return type, not declaration */
-		declaration = NULL;
+		if (inner_types != NULL) {
+			/* All later declarators only modify the return type, not declaration */
+			declaration = NULL;
+		}
 		rem_anchor_token(')');
 		expect(')');
 		break;
