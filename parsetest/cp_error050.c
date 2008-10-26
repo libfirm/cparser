36d0b4705bd7ca30516baf2861a5fb93832cc0ab
Matthias Braun <matze@braunis.de>
2008-10-26 18:04:37 +0000

    slightly improve testcase
    [r23213]

--- parsetest/cp_error050.c
+++ parsetest/cp_error050.c
@@ -1,5 +1,5 @@
 int main(void)
 {
 	// This is sizeof of a compound literal, not sizeof a type followed by garbage
-	return !sizeof(int[3]){ 0, 1, 2 };
+	return sizeof(int[]){ 0, 1, 2 } != 3*sizeof(int);
 }
