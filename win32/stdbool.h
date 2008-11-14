60c05987e01592bebb403b30ce6d74910ed8a1c0
Michael Beck <beck@ipd.info.uni-karlsruhe.de>
2008-11-14 23:09:53 +0000

    - use _Bool for C99 or intel compiler issue a lot of warnings
    [r23671]

--- win32/stdbool.h
+++ win32/stdbool.h
@@ -7,10 +7,17 @@
 
 #ifndef __cplusplus
 
+#ifdef __INTEL_COMPILER
+#define bool	_Bool
+#define true	((_Bool)1)
+#define false	((_Bool)0)
+#else
+
 #define bool	int
 #define true	1
 #define false	0
 
+#endif
 #endif /* __cplusplus */
 
 #endif	/* stdbool.h */
