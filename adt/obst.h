4d7a19a3863ef2be82bd43be8227385d1b52372b
Matthias Braun <matze@braunis.de>
2010-05-04 13:14:07 +0000

    avoid code duplication in cparser by using libfirms obstack
    [r27480]

--- adt/obst.h
+++ adt/obst.h
@@ -17,8 +17,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
  * 02111-1307, USA.
  */
-#include "obstack.h"
 #include "xmalloc.h"
+#include <libfirm/adt/obstack.h>
 
 #define obstack_chunk_alloc xmalloc
 #define obstack_chunk_free  free
