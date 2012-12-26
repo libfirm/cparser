3b062e5517f7cf803e65d81ffbe10ab9fe415c3a
Christoph Mallon <christoph.mallon@gmx.de>
2012-12-26 15:43:13 +0100

    cleanup: Remove stale math includes.

--- ast.c
+++ ast.c
@@ -19,14 +19,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#if defined(__INTEL_COMPILER)
-#include <mathimf.h>
-#elif defined(__CYGWIN__)
-#include "win32/cygwin_math_ext.h"
-#else
-#include <math.h>
-#endif
-
 #include "adt/error.h"
 #include "adt/util.h"
 
