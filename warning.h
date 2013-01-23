391b7f8699fc25df3e9f2862cfac78119345e01b
Matthias Braun <matze@braunis.de>
2013-01-23 11:22:55 +0100

    remove unimplemented/commented warning definitions
    You can add them back once you implement the warnings.

--- warning.h
+++ warning.h
@@ -17,20 +17,11 @@ typedef enum warning_t {
 	WARN_ADDRESS,                       /**< Warn about suspicious uses of memory addresses */
 	WARN_AGGREGATE_RETURN,              /**< Warn if any functions that return structures or unions are defined or called */
 	WARN_ATTRIBUTE,                     /**< Warn if an unexpected `__attribute__' is used or function attributes applied to variables, etc. */
-#if 0 // TODO
-	WARN_BAD_FUNCTION_CAST,             /**< Warn whenever a function call is cast to a non-matching type */
-#endif
 	WARN_BUILTIN_MACRO_REDEFINED,       /**< warn when undefining/redefining builtin macros */
-#if 0
-	WARN_CAST_ALIGN,                    /**< Warn whenever a pointer is cast such that the required alignment of the target is increased */
-#endif
 	WARN_CAST_QUAL,                     /**< Warn whenever a pointer is cast so as to remove a type qualifier from the target type */
 	WARN_CHAR_SUBSCRIPTS,               /**< Warn if an array subscript has the type 'char' */
 	WARN_COMMENT,                       /**< Warn whenever a comment-start sequence appears in a comment, or whenever a Backslash-Newline appears in a '//' comment. */
 	WARN_CONVERSION,                    /**< Warn if a prototype causes a type conversion that is different from what would happen to the same argument in the absence of a prototype */
-#if 0 // TODO
-	WARN_CPP_COMPAT,                    /**< Warn about ISO C constructs that are outside of the common subset of ISO C and ISO C++. */
-#endif
 	WARN_DECLARATION_AFTER_STATEMENT,   /**< Warn when a declaration is found after a statement in a block */
 	WARN_DEPRECATED_DECLARATIONS,       /* TODO implement for types */ /**< Warn about uses of functions, variables and types marked as deprecated by using the 'deprecated' attribute */
 	WARN_DIV_BY_ZERO,                   /**< Warn about compile-time integer division by zero */
@@ -44,20 +35,9 @@ typedef enum warning_t {
 	WARN_IGNORED_QUALIFIERS,            /**< Warn when type qualifiers are meaningless */
 	WARN_IMPLICIT_FUNCTION_DECLARATION, /**< Warn whenever a function is used before being declared */
 	WARN_IMPLICIT_INT,                  /**< Warn when a declaration does not specify a type */
-#if 0 // TODO
-	WARN_INLINE,                        /**< Warn if a function can not be inlined and it was declared as inline */
-	WARN_INT_TO_POINTER_CAST,           /**< Warn if cast from integer to pointer of different size. */
-#endif
 	WARN_LONG_LONG,                     /**< Warn if 'long long' type is used */
 	WARN_MAIN,                          /**< Warn if the type of 'main' is suspicious */
-#if 0 // TODO
-	WARN_MISSING_BRACES,                /**< Warn if an aggregate or union initializer is not fully bracketed */
-#endif
 	WARN_MISSING_DECLARATIONS,          /**< Warn if a global function is defined without a previous declaration */
-#if 0 // TODO
-	WARN_MISSING_FIELD_INITIALIZERS,    /**< Warn if a structure's initializer has some fields missing. */
-	WARN_MISSING_FORMAT_ATTRIBUTE,      /**< If '-Wformat' is enabled, also warn about functions which might be candidates for 'format' attributes */
-#endif
 	WARN_MISSING_NORETURN,              /**< Warn about functions which might be candidates for attribute 'noreturn' */
 	WARN_MISSING_PROTOTYPES,            /**< Warn if a global function is defined without a previous prototype declaration */
 	WARN_MULTICHAR,                     /**< Warn if a multicharacter constant ('FOOF') is used. */
@@ -69,21 +49,12 @@ typedef enum warning_t {
 	WARN_PADDED,                        /**< Warn if padding is included in a structure, either to align an element of the structure or to align the whole structure */
 	WARN_PARENTHESES,                   /**< Warn if parentheses are omitted in certain contexts (assignment where truth value is expected, if-else-braces) */
 	WARN_POINTER_ARITH,                 /**< Warn about anything that depends on the "size of" a function type or of 'void' */
-#if 0 // TODO
-	WARN_POINTER_TO_INT_CAST,           /**< Warn if cast from pointer to integer of different size. */
-#endif
 	WARN_REDUNDANT_DECLS,               /**< Warn about redundant declarations */
 	WARN_RETURN_TYPE,                   /* TODO not fully implemented */ /**< Warn about function definitions with a return-type that defaults to 'int'.  Also warn about any 'return' statement with no return-value in a function whose return-type is not 'void'. */
-#if 0 // TODO
-	WARN_SEQUENCE_POINT,                /**< Warn about code that may have undefined semantics because of violations of sequence point rules */
-#endif
 	WARN_SHADOW,                        /**< Warn whenever a local variable shadows another local variable, parameter or global variable or whenever a built-in function is shadowed */
 	WARN_SHADOW_LOCAL,
 	WARN_SIGN_COMPARE,                  /**< Warn when a comparison between signed and unsigned values could produce an incorrect result when the signed value is converted to unsigned */
 	WARN_STRAY_SEMICOLON,               /**< Warn about stray semicolons outside of functions */
-#if 0 // TODO
-	WARN_STRICT_ALIASING,               /**< Warn about code which might break the strict aliasing rules that the compiler is using for optimization. */
-#endif
 	WARN_STRICT_PROTOTYPES,             /**< Warn if a function declaration has an unspecified parameter list */
 	WARN_SWITCH_DEFAULT,                /**< Warn whenever a 'switch' statement does not have a 'default' case */
 	WARN_SWITCH_ENUM,                   /**< Warn about 'switch' statements with an enum as index type and missing case labels or case labels outside the enum range TODO has an alias -Wswitch? */
