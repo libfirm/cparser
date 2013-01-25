e57298b660eb62f91d9d999f6f5a9c569b432921
Christoph Mallon <christoph.mallon@gmx.de>
2013-01-25 09:48:17 +0100

    preprocessor: Use DIGIT_CASES.

--- preprocessor.c
+++ preprocessor.c
@@ -1347,16 +1347,7 @@ restart:
 
 	case '.':
 		MAYBE_PROLOG
-			case '0':
-			case '1':
-			case '2':
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9':
+			case DIGIT_CASES:
 				put_back('.');
 				parse_number();
 				return;
