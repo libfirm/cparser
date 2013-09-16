2a868ed0d0301e6cfa06a803834c2da272ea249f
Matthias Braun <matze@braunis.de>
2013-09-15 19:16:02 -0700

    move help strings from main.c to help.c

--- help.h
+++ help.h
@@ -15,4 +15,24 @@ void put_help(const char *option, const char *explanation);
  */
 void put_choice(const char *choice, const char *explanation);
 
+typedef enum {
+	HELP_NONE          = 0,
+	HELP_BASIC         = 1 << 0,
+	HELP_PREPROCESSOR  = 1 << 1,
+	HELP_PARSER        = 1 << 2,
+	HELP_WARNINGS      = 1 << 3,
+	HELP_OPTIMIZATION  = 1 << 4,
+	HELP_CODEGEN       = 1 << 5,
+	HELP_LINKER        = 1 << 6,
+	HELP_LANGUAGETOOLS = 1 << 7,
+	HELP_DEBUG         = 1 << 8,
+	HELP_FIRM          = 1 << 9,
+
+	HELP_ALL           = -1
+} help_sections_t;
+
+void help_print(const char *argv0, help_sections_t sections);
+
+void help_usage(const char *argv0);
+
 #endif
