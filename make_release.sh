55b49df067cb9f2914efb01986c38c19a779e784
Matthias Braun <matze@braunis.de>
2016-01-02 11:20:34 -0800

    make_release: Create .gz and .xz variants

--- make_release.sh
+++ make_release.sh
@@ -13,7 +13,6 @@ VERSION_MAJOR="1"
 VERSION_MINOR="22"
 VERSION_MICRO="0"
 VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO}"
-RELEASEFILE="build/cparser-$VERSION.tar.bz2"
 VERSIONFILE=src/driver/version.h
 
 # test if versions match
@@ -26,6 +25,17 @@ grep -q "set(cparser_VERSION \"${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_MICRO
 echo "Checking version in NEWS.md"
 egrep -q "$VERSION_MAJOR.$VERSION_MINOR.$VERSION_MICRO" NEWS.md
 
-echo "creating $RELEASEFILE"
-mkdir -p "$(dirname "$RELEASEFILE")"
-git archive --prefix cparser-$VERSION/ HEAD | bzip2 > "$RELEASEFILE"
+RELEASEBZ2="build/cparser-$VERSION.tar.bz2"
+echo "creating $RELEASEBZ2"
+mkdir -p "$(dirname "$RELEASEBZ2")"
+git archive --prefix cparser-$VERSION/ HEAD | bzip2 -9 > "$RELEASEBZ2"
+
+RELEASEGZ="build/cparser-$VERSION.tar.gz"
+echo "creating $RELEASEGZ"
+mkdir -p "$(dirname "$RELEASEGZ")"
+git archive --prefix cparser-$VERSION/ HEAD | gzip -9 > "$RELEASEGZ"
+
+RELEASEXZ="build/cparser-$VERSION.tar.xz"
+echo "creating $RELEASEXZ"
+mkdir -p "$(dirname "$RELEASEXZ")"
+git archive --prefix cparser-$VERSION/ HEAD | xz -9 > "$RELEASEXZ"
